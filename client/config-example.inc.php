189095c8446136de6dd5e1cd1236254739e14161
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-06-22 12:36:15 +0200

    feat(add-env-file): add newline and remove X

--- client/config-example.inc.php
+++ client/config-example.inc.php
@@ -1,10 +1,10 @@
 <?php
 
-define('OAUTH_CLIENT_ID', 'xxx');
-define('OAUTH_CLIENT_SECRET', 'xxx');
-define('FACEBOOK_CLIENT_ID', 'xxx');
-define('FACEBOOK_CLIENT_SECRET', 'xxx');
-define('DISCORD_CLIENT_ID', 'xxx');
-define('DISCORD_CLIENT_SECRET', 'xxx');
-define('GITHUB_CLIENT_ID', 'xxx');
-define('GITHUB_CLIENT_SECRET', 'xxxx');
+define('OAUTH_CLIENT_ID', '');
+define('OAUTH_CLIENT_SECRET', '');
+define('FACEBOOK_CLIENT_ID', '');
+define('FACEBOOK_CLIENT_SECRET', '');
+define('DISCORD_CLIENT_ID', '');
+define('DISCORD_CLIENT_SECRET', '');
+define('GITHUB_CLIENT_ID', '');
+define('GITHUB_CLIENT_SECRET', '');
